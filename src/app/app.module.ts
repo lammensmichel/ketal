848aea74e4b7f833d01c50eea8c8d7cc082cdd70
lammensmichel <lammens.michel@gmail.com>
2023-10-13 14:20:10 +0200

    add game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,11 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PlayersListComponent } from 'src/players/players-list/players-list.component';
 import { Routes } from '@angular/router';
-
-
+import { GameComponent } from '../game/game.component';
 
 @NgModule({
-  declarations: [AppComponent, PlayersListComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-  ],
+  declarations: [AppComponent, PlayersListComponent, GameComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
