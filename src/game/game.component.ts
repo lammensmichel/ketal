ccb48c60ad55487ed086438b9fd446886ebe7bd5
knabo6 <knabo6@gmail.com>
2023-10-15 10:50:46 +0200

    Fix keydown event error
    Fix keydown event called on first button when already in part 2 of the game

--- src/game/game.component.ts
+++ src/game/game.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { CardDeckHelperService } from 'src/helpers/card-deck.helper';
-import { PlayerHelperService } from 'src/helpers/player.helper';
-import { CardType } from 'src/models/card-type.model';
-import { PlayerModel } from 'src/models/player.model';
+import {Component, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
+import {CardDeckHelperService} from 'src/helpers/card-deck.helper';
+import {PlayerHelperService} from 'src/helpers/player.helper';
+import {CardType} from 'src/models/card-type.model';
+import {PlayerModel} from 'src/models/player.model';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +10,14 @@ import { PlayerModel } from 'src/models/player.model';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key === 'B' || event.key === 'b' || event.key === 'D' || event.key === 'd') {
+      // Handle 'B' or 'b' key press here
+      this.onNextClick();
+    }
+  }
+
   @Output() public goToPhaseTwo: EventEmitter<void> = new EventEmitter<void>();
   public playerCount: number = 0;
   public maxTurnCount: number = 0;
@@ -18,15 +26,15 @@ export class GameComponent implements OnInit {
   public goToSecondPhase: boolean = false;
   public currentCard: CardType | undefined;
 
-  players: PlayerModel[] = [ ];
+  players: PlayerModel[] = [];
 
   constructor(playerHelperService: PlayerHelperService, public cardDeckHelperService: CardDeckHelperService) {
     this.players = playerHelperService.players;
   }
 
   public ngOnInit(): void {
-      this.playerCount = this.players.length;
-      this.maxTurnCount = this.playerCount * 4;
+    this.playerCount = this.players.length;
+    this.maxTurnCount = this.playerCount * 4;
   }
 
   public getPlayerName(): string {
@@ -34,18 +42,18 @@ export class GameComponent implements OnInit {
   }
 
   onNextClick() {
+    if (this.turnCount === this.maxTurnCount) return;
     this.currentCard = this.cardDeckHelperService.getRandomCard();
-
 
     this.players[this.playerTurn].cards.push(this.currentCard);
     this.playerTurn++;
     this.turnCount++;
 
-    if(this.playerTurn === this.playerCount){
+    if (this.playerTurn === this.playerCount) {
       this.playerTurn = 0;
     }
 
-    if(this.turnCount === this.maxTurnCount){
+    if (this.turnCount === this.maxTurnCount) {
       this.goToSecondPhase = true;
       this.goToPhaseTwo.emit();
     }
@@ -54,23 +62,23 @@ export class GameComponent implements OnInit {
   public getMessage(): string {
     let message = '';
 
-    if(this.turnCount < this.playerCount){
+    if (this.turnCount < this.playerCount) {
       message = 'Rouge ou noir: ';
     }
 
-    if(this.turnCount >= this.playerCount && this.turnCount < this.playerCount *2) {
+    if (this.turnCount >= this.playerCount && this.turnCount < this.playerCount * 2) {
 
       message = 'Plus grand ou plus petit: ';
     }
 
-    if(this.turnCount >= this.playerCount*2 && this.turnCount < this.playerCount *3) {
+    if (this.turnCount >= this.playerCount * 2 && this.turnCount < this.playerCount * 3) {
 
       message = 'Entre ou extérieur: ';
     }
 
-    if(this.turnCount >= this.playerCount*3) {
+    if (this.turnCount >= this.playerCount * 3) {
 
-      message = 'Carreau Coeur rèfle ou Pique: ';
+      message = 'Coeur, Carreau, Pique ou trèfle ';
     }
 
     return message;
--- src/second-phase/second-phase.component.ts
+++ src/second-phase/second-phase.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { CardDeckHelperService } from 'src/helpers/card-deck.helper';
-import { PlayerHelperService } from 'src/helpers/player.helper';
-import { CardType } from 'src/models/card-type.model';
-import { PlayerModel } from 'src/models/player.model';
+import {Component, HostListener} from '@angular/core';
+import {CardDeckHelperService} from 'src/helpers/card-deck.helper';
+import {PlayerHelperService} from 'src/helpers/player.helper';
+import {CardType} from 'src/models/card-type.model';
+import {PlayerModel} from 'src/models/player.model';
 
 @Component({
   selector: 'app-second-phase',
@@ -10,6 +10,21 @@ import { PlayerModel } from 'src/models/player.model';
   styleUrls: ['./second-phase.component.scss'],
 })
 export class secondPhaseComponent {
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.key === 'B' || event.key === 'b') {
+      // Handle 'B' or 'b' key press here
+      if (this.numSips < 13) {
+        this.onDisplayCard();
+      }
+    } else if (event.key === 'D' || event.key === 'd') {
+      // Handle 'D' or 'd' key press here
+      if (this.numSips < 13) {
+        this.onDisplayCard();
+      }
+    }
+  }
+
   public numSips: number = 1;
   public cptNumSips: number = 1;
 
@@ -25,7 +40,7 @@ export class secondPhaseComponent {
     this.players = playerHelperService.players;
   }
 
-  onNextClick() {
+  onDisplayCard() {
     let card = this.cardDeckHelperService.getRandomCard();
     card.Sips = this.cptNumSips;
 
