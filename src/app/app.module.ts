ada55d57a77cf77a800789bcae66f6ec431d975f
lammensmichel <lammens.michel@gmail.com>
2023-10-21 20:02:43 +0200

    begin room

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PlayersListComponent } from 'src/players/players-list/players-list.component';
 import { PlayerHelperService } from 'src/helpers/player.helper';
 import { CardDeckHelperService } from 'src/helpers/card-deck.helper';
@@ -13,6 +13,11 @@ import { LocalService } from 'src/app/services/local/local.service';
 import { PlayingCardComponent } from './playing-card/playing-card.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { GameRoomComponent } from 'src/game-room/game-room.component';
+import { WebsocketService } from 'src/websocket/websocket.service';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -23,9 +28,21 @@ import { HeaderComponent } from './header/header.component';
     PlayingCardComponent,
     FooterComponent,
     HeaderComponent,
+    GameRoomComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [PlayerHelperService, CardDeckHelperService, LocalService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+  ],
+  providers: [
+    PlayerHelperService,
+    CardDeckHelperService,
+    LocalService,
+    WebsocketService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
