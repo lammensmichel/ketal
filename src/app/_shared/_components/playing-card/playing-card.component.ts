38be924d839b761bf6927ee032ea4f0f90ba52fc
Solacin21 <the.alien14@gmail.com>
2023-10-23 14:59:03 +0200

    refactoring css
    css removal
    
    css
    
    css
    
    refactor css

--- src/app/_shared/_components/playing-card/playing-card.component.ts
+++ src/app/_shared/_components/playing-card/playing-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CardType } from '../../_models/card-type.model';
 
 @Component({
